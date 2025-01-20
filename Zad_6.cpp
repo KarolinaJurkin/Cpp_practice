9d09794553b6090c3d5b1a58c4ca8aca2cab352c
Karolina Jurkin <corpsecally@gmail.com>
2025-01-20 10:12:58 +0100

    Programowanie - Zad_6 draft
    1. Do tablicy A[w][k] (w, k - stałe) zawierającej rekordy - dane studentów o polach: 
    - dwa pola typu char oznaczające inicjały studenta 
    - pole typu double oznaczające ocenę punktową z egzaminu 
    wylosować rekordy w taki sposób, aby inicjały były dużymi literami, zaś oceny były z zakresu od 0 do stałej G.
    2. Tablicę wydrukować wierszami w postaci kolejnych rekordów, w czytelny sposób, z dokładnością do 2 miejsc po przecinku w polu liczbowym, np. WL 25.05 FA 7.99 SM 15.75 
    3. Przesunąć cyklicznie o jedno miejsce w lewo wiersze zaczynające się rekordami z inicjałami imion A lub B. 
    4. Ponownie wydrukować wierszami tablicę.

---
+++ Zad_6.cpp
@@ -0,0 +1,54 @@
+#include <streamio>
+#include <ctime>
+#include <iomanip>
+using namespace std;
+
+int	main(void)
+{
+	const int	w = 4, k = 5, G = 5;
+	struct		Sstudent
+	{
+		char	ini_1, ini_2;
+		double	ocena;
+	};
+	Sstudent	A[w][k];
+	
+	srand(time(0));
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			A[i][j].ini_1 = 64 + rand() % (91 - 65);
+			A[i][j].ini_2 = 64 + rand() % (91 - 65);
+			A[i][j].ocena = rand() % 6;
+			cout << A[i][j].ini_1 << A[i][j].ini_2 << " ";
+			cout << fixed << setprecision(2) << setw(5) << A[i][j].ocena;
+		}
+		cout << endl;
+	}
+	
+	cout << endl;
+
+	for (int i = 0; i < w; i++)
+	{
+		for (int j = 0; j < k; j++)
+		{
+			if (A[i][j].ini_1 == 'A' || A[i][j].ini_1 == 'B')
+			{
+				for (int l = 0; l < k - 1; l++)
+				{
+					A[i][l].ini_1 = A[i][l + 1].ini_1;
+					A[i][l].ini_2 = A[i][l + 1].ini_2;
+					A[i][l].ocena = A[i][l + 1].ocena;
+				}
+				A[i][k] = A[i][0].ini_1;
+				A[i][k] = A[i][0].ini_2;
+				A[i][k] = A[i][0].ocena;
+			}
+			cout << A[i][j].ini_1 << A[i][j].ini_2 << " ";
+			cout << fixed << setprecision(2) << setw(5) << A[i][j].ocena;
+		}
+		cout << endl;
+	}
+	return (0);
+}
