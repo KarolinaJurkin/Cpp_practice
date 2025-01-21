2479b42bcd9e782f3445917f0c92acc175cfbddc
Karolina Jurkin <corpsecally@gmail.com>
2025-01-21 09:44:35 +0100

    Programowanie - Zad_6.cpp
    1. Do tablicy A[w][k] (w, k - stałe) zawierającej rekordy - dane studentów o polach: 
    - dwa pola typu char oznaczające inicjały studenta 
    - pole typu double oznaczające ocenę punktową z egzaminu 
    wylosować rekordy w taki sposób, aby inicjały były dużymi literami, zaś oceny były z zakresu od 0 do stałej G.
    2. Tablicę wydrukować wierszami w postaci kolejnych rekordów, w czytelny sposób, z dokładnością do 2 miejsc po przecinku w polu liczbowym, np. WL 25.05 FA 7.99 SM 15.75 
    3. Przesunąć cyklicznie o jedno miejsce w lewo wiersze zaczynające się rekordami z inicjałami imion A lub B. 
    4. Ponownie wydrukować wierszami tablicę.

--- Zad_6.cpp
+++ Zad_6.cpp
@@ -1,4 +1,4 @@
-#include <streamio>
+#include <iostream>
 #include <ctime>
 #include <iomanip>
 using namespace std;
@@ -19,35 +19,40 @@ int	main(void)
 	{
 		for (int j = 0; j < k; j++)
 		{
-			A[i][j].ini_1 = 64 + rand() % (91 - 65);
-			A[i][j].ini_2 = 64 + rand() % (91 - 65);
+			A[i][j].ini_1 = 65 + rand() % (91 - 65);
+			A[i][j].ini_2 = 65 + rand() % (91 - 65);
 			A[i][j].ocena = 6 * rand() / (double) RAND_MAX;
 			cout << A[i][j].ini_1 << A[i][j].ini_2 << " ";
-			cout << fixed << setprecision(2) << setw(5) << A[i][j].ocena;
+			cout << fixed << setprecision(2) << A[i][j].ocena << " ";
 		}
 		cout << endl;
 	}
 	
 	cout << endl;
 
+    char    temp1, temp2; 
+    double  tempo;
 	for (int i = 0; i < w; i++)
 	{
 		for (int j = 0; j < k; j++)
 		{
-			if (A[i][j].ini_1 == 'A' || A[i][j].ini_1 == 'B')
+			if (A[i][0].ini_1 == 'A' || A[i][0].ini_1 == 'B')
 			{
+                temp1 = A[i][0].ini_1;
+                temp2 = A[i][0].ini_2;
+                tempo = A[i][0].ocena;
 				for (int l = 0; l < k - 1; l++)
 				{
 					A[i][l].ini_1 = A[i][l + 1].ini_1;
 					A[i][l].ini_2 = A[i][l + 1].ini_2;
 					A[i][l].ocena = A[i][l + 1].ocena;
 				}
-				A[i][k] = A[i][0].ini_1;
-				A[i][k] = A[i][0].ini_2;
-				A[i][k] = A[i][0].ocena;
+				A[i][k - 1].ini_1 = temp1;
+				A[i][k - 1].ini_2 = temp2;
+				A[i][k - 1].ocena = tempo;
 			}
 			cout << A[i][j].ini_1 << A[i][j].ini_2 << " ";
-			cout << fixed << setprecision(2) << setw(5) << A[i][j].ocena;
+			cout << fixed << setprecision(2) << A[i][j].ocena << " ";
 		}
 		cout << endl;
 	}
